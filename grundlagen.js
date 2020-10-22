a819480e174b4ccd28417dc348db2e2f564ef377
meisterk <franz@kohnlehome.de>
2020-10-22 08:59:37 +0200

    Add arrays

--- grundlagen.js
+++ grundlagen.js
@@ -1,1 +1,21 @@
+// Ausgabe
 console.log('Hallo');
+
+// Variablen und Datentypen
+const zahl1 = 3.5; // number
+const zahl2 = -17; // number
+
+const text1 = 'Blabla'; // string
+const text2 = `Blabla ${zahl1} ist vom Typ ${typeof (zahl1)}`; // string
+
+const wahr = (1 > 2); // boolean
+
+// Arrays []
+const namen = ['Anna', 'Berta', 'Carla']; // object
+const anzahl = namen.length; // 3
+namen[3] = 'Dieter';
+
+for (let i = 0; i < namen.length; i++) {
+    console.log(namen[i]);
+}
+
